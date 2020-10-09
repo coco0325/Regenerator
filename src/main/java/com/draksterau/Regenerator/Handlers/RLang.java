d9b3cb1f01d6d6610f9dd813ccd1444faa2ed859
Mr_Coco <hihicoco0325@gmail.com>
2020-10-09 10:19:41 +0800

    update

---
+++ src/main/java/com/draksterau/Regenerator/Handlers/RLang.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.draksterau.Regenerator.Handlers;
+
+import com.draksterau.Regenerator.RegeneratorPlugin;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+/**
+ *
+ * @author draks
+ */
+public final class RLang extends RObject {
+
+    private File langConfigFile;
+    private FileConfiguration langConfig;
+    private File langConfigFileTemp;
+    private FileConfiguration langConfigTemp;
+    private String language;
+    
+    public RLang(RegeneratorPlugin plugin, String language) {
+        super(plugin);
+        this.language = language;
+        this.loadData();
+    }
+    
+    @Override
+    void loadData() {
+        // Attempt to load the config file.
+        langConfigFile = new File(plugin.getDataFolder() + "/lang/" + this.language + ".yml");
+        // If the config file is null (due to the config file being invalid or not there) create a new one.
+        // If the file doesnt exist, populate it from the template.
+        if (!langConfigFile.exists()) {
+            langConfigFile = new File(plugin.getDataFolder() + "/lang/" + this.language + ".yml");
+                if (plugin.getResource(this.language + ".yml") != null) {
+                    langConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(plugin.getResource(this.language + ".yml")));
+                } else {
+                    plugin.utils.throwMessage(MsgType.WARNING,"Language: " + language + " is not available. Defaulting back to ENGLISH...");
+                    this.language = "ENGLISH";
+                    langConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(plugin.getResource(this.language + ".yml")));
+                }
+            saveData();
+        }
+        // Attempt to read the config in the config file.
+        langConfig = YamlConfiguration.loadConfiguration(langConfigFile);
+    }
+    
+    public String getLanguage() {
+        return this.language;
+    }
+    
+    public void setLanguage(String s) {
+        this.language = s;
+    }
+    
+    public YamlConfiguration getDefaultLanguage() {
+        return YamlConfiguration.loadConfiguration(new InputStreamReader(plugin.getResource("ENGLISH.yml")));
+    }
+
+    public String getForKey(String key) {
+            try {
+                String value = langConfig.getString(key);
+                if (value == null) {
+                    langConfigFileTemp = new File(plugin.getDataFolder() + "/lang/" + this.language + ".yml");
+                    langConfigTemp = YamlConfiguration.loadConfiguration(new InputStreamReader(plugin.getResource(this.language + ".yml")));
+                    if (langConfigTemp.isSet(key)) {
+                        plugin.utils.throwMessage(MsgType.NEW,String.format(this.getForKey("messages.langMissingEntries"),key,plugin.getDataFolder() + "/lang/" + this.language + ".yml"));
+                        String tempValue = langConfigTemp.getString(key);
+                        langConfig.set(key, tempValue);
+                        this.saveData();
+                        return tempValue;
+                    } else {
+                        langConfigTemp = YamlConfiguration.loadConfiguration(new InputStreamReader(plugin.getResource("ENGLISH.yml")));
+                        if (langConfigTemp.isSet(key)) {
+                            plugin.utils.throwMessage(MsgType.SEVERE,"Loading default lang value for : " + key + " from ENGLISH language, as " + language + " does not have one set.");
+                            String tempValue = langConfigTemp.getString(key);
+                            langConfig.set(key, tempValue);
+                            this.saveData();
+                            return tempValue;
+                        } else {
+                            plugin.utils.throwMessage(MsgType.SEVERE, "Could not locate lang key for : " + key + " in either language: " + language + " or ENGLISH!");
+                            return "Error.LangKey." + key;
+                        }
+                    }
+                } else {
+                    return value;
+                }
+            } catch (Exception e) {
+                plugin.utils.throwMessage(MsgType.SEVERE,"Language file : " + langConfigFile.getAbsolutePath() + " is invalid. Exception: " + e.getMessage() + "!");
+                if (plugin.config.debugMode) e.printStackTrace();
+                plugin.disablePlugin();
+                return null;
+            }
+    }
+    
+    @Override
+    void saveData() {
+        try {
+            langConfig.save(langConfigFile);
+        } catch (IOException ex) {
+            plugin.utils.throwMessage(MsgType.SEVERE,String.format(this.getForKey("messages.cantSaveLang"), langConfigFile.getAbsolutePath(), ex.getMessage()));
+            if (plugin.config.debugMode) ex.printStackTrace();
+        }    
+    }
+
+}
---
+++ src/main/java/com/draksterau/Regenerator/tasks/ChunkTask.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.draksterau.Regenerator.tasks;
+
+import com.draksterau.Regenerator.Handlers.MsgType;
+import com.draksterau.Regenerator.Handlers.RChunk;
+import com.draksterau.Regenerator.event.RegenerationActionEvent;
+import static java.lang.Math.random;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.generator.BlockPopulator;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ *
+ * @author draks
+ */
+public class ChunkTask extends BukkitRunnable {
+    
+    private boolean wasUnloaded;
+    RChunk RChunk;
+    private Logger log = Logger.getLogger("Minecraft");
+    private boolean isManual;
+    
+    public ChunkTask (RChunk RChunk, boolean isManual) {
+        this.RChunk = RChunk;
+        this.isManual = isManual;
+    }
+    
+    
+    
+    @Override
+    
+    public void run() {   
+            RChunk.plugin.utils.throwMessage(MsgType.DEBUG, "Beginning chunk regeneration task for : " + RChunk.chunkX + "," + RChunk.chunkZ + " on world : " + RChunk.getWorldName() + "...");
+            if (!isManual) {
+                // Lets check if the world the chunk is on is loaded still.
+                if (Bukkit.getServer().getWorld(RChunk.worldName) == null) {
+                    RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.skippingRegenWorldUnloaded"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+                    return;
+                }
+                
+                // Now fires an action event for other plugins to cancel.
+                
+                // Creating fake location for the regeneration event. For future region based support.
+                Location location = new Location(this.RChunk.getWorld(), (RChunk.chunkX*16), 0.0, (RChunk.chunkZ*16));
+                RegenerationActionEvent actionEvent = new RegenerationActionEvent(location);
+                
+                Bukkit.getServer().getPluginManager().callEvent(actionEvent);
+                if (actionEvent.isCancelled()) {
+                    int reasonCount = 1;                                            
+                    RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.skippingRegenDueToActionEventCancellation"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+                    for (String s : actionEvent.getCancelledReasons().keySet()) {
+                        RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.skippingRegenActionEventResult"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName(), reasonCount, s, actionEvent.getCancelledReasons().get(s).getName()));
+                        reasonCount++;
+                    }
+                    RChunk.resetActivity();
+                    return;        
+                }
+                // Now checking if a chunk is claimed at the point of regenerating only.
+                if (!RChunk.plugin.utils.autoRegenRequirementsMet(RChunk.getChunk())) {
+                    RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.skippingRegenAutoRegenRequirementFail"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName(),RChunk.plugin.config.distanceNearbyMinimum));
+                    RChunk.resetActivity();
+                    return;
+                }
+                if (!RChunk.plugin.utils.isLagOK()) {
+                    RChunk.plugin.utils.throwMessage(MsgType.WARNING, String.format(RChunk.plugin.lang.getForKey("messages.skippingRegenTPSToLow"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName(),RChunk.plugin.config.minTpsRegen));
+                    return;
+                }
+                if (!RChunk.plugin.utils.getPlayersNearChunk(RChunk, RChunk.plugin.config.distanceNearbyMinimum).isEmpty()) {
+                    RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.distanceNearbyMinimum"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName(),RChunk.plugin.config.distanceNearbyMinimum));
+                    return;
+                }
+                if (RChunk.getChunk().isLoaded() && !RChunk.plugin.config.targetLoadedChunks) {
+                    RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.skippingRegenOfLoadedChunk"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+                   return;
+                }
+                if (!RChunk.getChunk().isLoaded() && !RChunk.plugin.config.targetUnloadedChunks) {
+                    RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.skippingRegenOfUnloadedChunk"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+                   return;
+                }
+                if (RChunk.getChunk().getWorld().isChunkInUse(RChunk.chunkX, RChunk.chunkZ) && !RChunk.plugin.config.regenerateChunksInUseByPlayers) {
+                    RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.skippingRegenOfChunkInUseByPlayers"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+                    return;
+                }
+            }
+            if (!RChunk.getChunk().isLoaded()) {
+            RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.loadingToRegen"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+                RChunk.getChunk().load();
+                wasUnloaded = true;
+            }
+            
+            RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.startingRegenChunk"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+            
+            if (RChunk.plugin.config.clearRegeneratedChunksOfEntities) {
+                RChunk.plugin.utils.throwMessage(MsgType.DEBUG, String.format(RChunk.plugin.lang.getForKey("messages.regenClearEntities"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+                RChunk.plugin.utils.clearEntitiesFromChunk(RChunk);
+            }
+            
+            if (RChunk.plugin.utils.regenerateChunk(RChunk.getChunk())) {
+                RChunk.plugin.utils.throwMessage(MsgType.SUCCESS, String.format(RChunk.plugin.lang.getForKey("messages.regenSuccess"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+            } else {
+                RChunk.plugin.utils.throwMessage(MsgType.SEVERE, String.format(RChunk.plugin.lang.getForKey("messages.regenFailed"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+            }
+            RChunk.resetActivity();
+            if (wasUnloaded) {
+                RChunk.plugin.utils.throwMessage(MsgType.INFO, String.format(RChunk.plugin.lang.getForKey("messages.unloadingChunkLoadedForRegen"), RChunk.getChunk().getX(), RChunk.getChunk().getZ(), RChunk.getWorldName()));
+                RChunk.getChunk().unload();
+            }
+    }
+}
+
